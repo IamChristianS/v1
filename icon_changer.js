8ee0ca7da00b0063f6994d2ff732927df666974e
Christian Santangelo <95893289+unbl0ck@users.noreply.github.com>
2022-12-19 12:52:14 -0600

    Update icon_changer.js

--- icon_changer.js
+++ icon_changer.js
@@ -1,14 +1,14 @@
 // Define the favicon URLs
 const faviconUrls = {
-  1: 'icons/canvas.ico',
-  2: 'icons/google-classroom.ico',
-  3: 'icons/google-drive.ico',
-  4: 'icons/google-mail.ico',
-  5: 'icons/google-slides.ico',
-  6: 'icons/google-docs.ico',
-  7: 'icons/google.ico',
-  8: 'icons/turnitin.ico',
-  9: 'icons/default.ico'
+  1: 'https://i.ibb.co/kDxcVDx/canvas.png',
+  2: 'https://i.ibb.co/YPyTYbJ/google-classroom.png',
+  3: 'https://i.ibb.co/JcgBjC1/google-drive.png',
+  4: 'https://i.ibb.co/5TfSsHK/google-mail.png',
+  5: 'https://i.ibb.co/n62414w/google-slides.png',
+  6: 'https://i.ibb.co/44MjCj9/google-docs.png',
+  7: 'https://i.ibb.co/6XW9nQ6/google.png',
+  8: 'https://i.ibb.co/CKrRjCG/turnitin.png',
+  9: 'https://i.ibb.co/KW8NVz7/default.png'
 };
 
 const buttons = document.querySelectorAll('button');
