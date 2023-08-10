1c5f5e3d39be397277a1a832680ad45cc2807646
unbl0ck <95893289+unbl0ck@users.noreply.github.com>
2023-08-10 00:17:09 -0500

    condensed repeated code

---
+++ startupScript.js
@@ -0,0 +1,15 @@
+// Disables Context (Right-Click) Menu
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Update Favicon & Title
+window.onload = function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = localStorage.getItem("faviconUrl");
+    document.getElementsByTagName('head')[0].appendChild(link);
+    var storedTitle = localStorage.getItem("title");
+    if (storedTitle) {
+        document.title = storedTitle;
+    }
+}
