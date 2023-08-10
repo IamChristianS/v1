158fc2618c207137914d2c78f8b42e503d749624
unbl0ck <95893289+unbl0ck@users.noreply.github.com>
2023-08-10 04:27:10 -0500

    added gamesArray.js
    doesnt do anything yet. this has been sitting on my computer, so ill just put it in the repo for safe-keeping

---
+++ gamesArray.js
@@ -0,0 +1,241 @@
+[{
+    "name": "Cookie Clicker",
+    "url": "https://unbl0ck.github.io/cookie/index.html",
+    "image": "https://unbl0ck.github.io/games/img/cookclck.png"
+}, {
+    "name": "2048",
+    "url": "https://unbl0ck.github.io/games/2048/index.html",
+    "image": "https://unbl0ck.github.io/games/img/2048.png"
+}, {
+    "name": "Mario",
+    "url": "https://unbl0ck.github.io/games/mario/index.html",
+    "image": "https://unbl0ck.github.io/games/img/mario.png"
+}, {
+    "name": "Flappy Bird",
+    "url": "https://unbl0ck.github.io/games/flappybird/index.html",
+    "image": "https://unbl0ck.github.io/games/img/flappybird.png"
+}, {
+    "name": "Tetris",
+    "url": "https://unbl0ck.github.io/games/tetris/index.html",
+    "image": "https://unbl0ck.github.io/games/img/tetris.png"
+}, {
+    "name": "World's Hardest Game",
+    "url": "https://unbl0ck.github.io/games/worldshardestgame/index.html",
+    "image": "https://unbl0ck.github.io/games/img/theworldshardestgame.png"
+}, {
+    "name": "The Impossible Quiz",
+    "url": "https://unbl0ck.github.io/games/theimpossiblequiz/index.html",
+    "image": "https://unbl0ck.github.io/games/img/theimpossiblequiz.png"
+}, {
+    "name": "Sonic",
+    "url": "https://unbl0ck.github.io/games/sonic/index.html",
+    "image": "https://unbl0ck.github.io/games/img/sonic.png"
+}, {
+    "name": "Super Mario 63",
+    "url": "https://unbl0ck.github.io/games/supermario63/index.html",
+    "image": "https://unbl0ck.github.io/games/img/supermario63.png"
+}, {
+    "name": "Breaking The Bank",
+    "url": "https://unbl0ck.github.io/games/henrystickmin/breakingthebank/index.html",
+    "image": "https://unbl0ck.github.io/games/img/breakingthebank.png"
+}, {
+    "name": "Escaping The Prison",
+    "url": "https://unbl0ck.github.io/games/henrystickmin/escapingtheprison/index.html",
+    "image": "https://unbl0ck.github.io/games/img/escapingtheprison.png"
+}, {
+    "name": "Stealing The Diamond",
+    "url": "https://unbl0ck.github.io/games/henrystickmin/stealingthediamond/index.html",
+    "image": "https://unbl0ck.github.io/games/img/stealingthediamond.png"
+}, {
+    "name": "Infiltrating The Airship",
+    "url": "https://unbl0ck.github.io/games/henrystickmin/infiltratingtheairship/index.html",
+    "image": "https://unbl0ck.github.io/games/img/infiltratingtheairship.png"
+}, {
+    "name": "Fleeing The Complex",
+    "url": "https://unbl0ck.github.io/games/henrystickmin/fleeingthecomplex/index.html",
+    "image": "https://unbl0ck.github.io/games/img/fleeingthecomplex.png"
+}, {
+    "name": "Slope",
+    "url": "https://unbl0ck.github.io/games/slope/index.html",
+    "image": "https://unbl0ck.github.io/games/img/slope.png"
+}, {
+    "name": "Riddle School 1",
+    "url": "https://unbl0ck.github.io/games/riddleschool/riddleschool1/index.html",
+    "image": "https://unbl0ck.github.io/games/img/riddleschool1.png"
+}, {
+    "name": "Riddle School 2",
+    "url": "https://unbl0ck.github.io/games/riddleschool/riddleschool2/index.html",
+    "image": "https://unbl0ck.github.io/games/img/riddleschool2.png"
+}, {
+    "name": "Riddle School 3",
+    "url": "https://unbl0ck.github.io/games/riddleschool/riddleschool3/index.html",
+    "image": "https://unbl0ck.github.io/games/img/riddleschool3.png"
+}, {
+    "name": "Riddle School 4",
+    "url": "https://unbl0ck.github.io/games/riddleschool/riddleschool4/index.html",
+    "image": "https://unbl0ck.github.io/games/img/riddleschool4.png"
+}, {
+    "name": "Riddle School 5",
+    "url": "https://unbl0ck.github.io/games/riddleschool/riddleschool5/index.html",
+    "image": "https://unbl0ck.github.io/games/img/riddleschool5.png"
+}, {
+    "name": "Riddle Transfer 1",
+    "url": "https://unbl0ck.github.io/games/riddletransfer1/index.html",
+    "image": "https://unbl0ck.github.io/games/img/riddletransfer1.png"
+}, {
+    "name": "Riddle Transfer 2",
+    "url": "https://unbl0ck.github.io/games/riddletransfer2/index.html",
+    "image": "https://unbl0ck.github.io/games/img/riddletransfer2.png"
+}, {
+    "name": "TransCube",
+    "url": "https://unbl0ck.github.io/games/dist/transcube/index.html",
+    "image": "https://unbl0ck.github.io/games/img/transcube.png"
+}, {
+    "name": "Minecraft",
+    "url": "https://unbl0ck.github.io/games/minecraft/index.html",
+    "image": "https://unbl0ck.github.io/games/img/minecraft.png"
+}, {
+    "name": "Run 3",
+    "url": "https://unbl0ck.github.io/games/run3/index.html",
+    "image": "https://unbl0ck.github.io/games/img/run3.png"
+}, {
+    "name": "MotoX3M",
+    "url": "https://unbl0ck.github.io/games/motox3m/index.html",
+    "image": "https://unbl0ck.github.io/games/img/motox3m.png"
+}, {
+    "name": "MotoX3M 2",
+    "url": "https://unbl0ck.github.io/games/motox3m2/index.html",
+    "image": "https://unbl0ck.github.io/games/img/motox3m2.png"
+}, {
+    "name": "Pokemon: Fire Red",
+    "url": "https://unbl0ck.github.io/launcher.html#pokemonred",
+    "image": "https://unbl0ck.github.io/games/img/pokemonfirered.png"
+}, {
+    "name": "Pokemon: Emerald",
+    "url": "https://unbl0ck.github.io/launcher.html#pokemonemerald",
+    "image": "https://unbl0ck.github.io/games/img/pokemonemerald.png"
+}, {
+    "name": "Monopoly",
+    "url": "https://unbl0ck.github.io/launcher.html#monopoly",
+    "image": "https://unbl0ck.github.io/games/img/monopoly.png"
+}, {
+    "name": "Spyro Adventure",
+    "url": "https://unbl0ck.github.io/launcher.html#spyro_adventure",
+    "image": "https://unbl0ck.github.io/games/img/spyroadventure.png"
+}, {
+    "name": "Street Fighter 2",
+    "url": "https://unbl0ck.github.io/launcher.html#super_street_fighter_2_turbo_revival",
+    "image": "https://unbl0ck.github.io/games/img/streetfighter2.png"
+}, {
+    "name": "Bloxors",
+    "url": "https://unbl0ck.github.io/games/bloxorz/index.html",
+    "image": "https://unbl0ck.github.io/games/img/bloxorz.png"
+}, {
+    "name": "Storm The House 2",
+    "url": "https://unbl0ck.github.io/games/stormthehouse2/index.html",
+    "image": "https://unbl0ck.github.io/games/img/stormthehouse2.png"
+}, {
+    "name": "Subway Surfers",
+    "url": "https://unbl0ck.github.io/games/subwaysurfers/index.html",
+    "image": "https://unbl0ck.github.io/games/img/subwaysurfers.png"
+}, {
+    "name": "Google Dino",
+    "url": "https://unbl0ck.github.io/games/googledino/index.html",
+    "image": "https://unbl0ck.github.io/games/img/googledino.png"
+}, {
+    "name": "FNaF 4",
+    "url": "https://unbl0ck.github.io/games/fnaf/fnaf4/index.html",
+    "image": "https://unbl0ck.github.io/games/img/fnaf4.png"
+}, {
+    "name": "Crossy Road",
+    "url": "https://unbl0ck.github.io/games/crossyroad/index.html",
+    "image": "https://unbl0ck.github.io/games/img/crossyroad.png"
+}, {
+    "name": "Cut The Rope",
+    "url": "https://unbl0ck.github.io/games/cuttherope/index.html",
+    "image": "https://unbl0ck.github.io/games/img/cuttherope.png"
+}, {
+    "name": "Stack",
+    "url": "https://unbl0ck.github.io/games/stack/index.html",
+    "image": "https://unbl0ck.github.io/games/img/stack.png"
+}, {
+    "name": "Duck Life 1",
+    "url": "https://unbl0ck.github.io/games/ducklife/ducklife1/index.html",
+    "image": "https://unbl0ck.github.io/games/img/ducklife1.png"
+}, {
+    "name": "Duck Life 2",
+    "url": "https://unbl0ck.github.io/games/ducklife/ducklife2/index.html",
+    "image": "https://unbl0ck.github.io/games/img/ducklife2.png"
+}, {
+    "name": "Duck Life 3",
+    "url": "https://unbl0ck.github.io/games/ducklife/ducklife3/index.html",
+    "image": "https://unbl0ck.github.io/games/img/ducklife3.png"
+}, {
+    "name": "Duck Life 4",
+    "url": "https://unbl0ck.github.io/games/ducklife/ducklife4/index.html",
+    "image": "https://unbl0ck.github.io/games/img/ducklife4.png"
+}, {
+    "name": "Super Mario 64",
+    "url": "https://unbl0ck.github.io/games/supermario64/index.html",
+    "image": "https://unbl0ck.github.io/games/img/supermario64.png"
+}, {
+    "name": "Learn To Fly 1",
+    "url": "https://unbl0ck.github.io/games/learntofly/learntofly1/index.html",
+    "image": "https://unbl0ck.github.io/games/img/learntofly1.png"
+}, {
+    "name": "Learn To Fly 2",
+    "url": "https://unbl0ck.github.io/games/learntofly/learntofly2/index.html",
+    "image": "https://unbl0ck.github.io/games/img/learntofly2.png"
+}, {
+    "name": "Papa's Burgeria",
+    "url": "https://unbl0ck.github.io/games/papas/papasburgeria/index.html",
+    "image": "https://unbl0ck.github.io/games/img/papasburgeria.png"
+}, {
+    "name": "Papa's Freezeria",
+    "url": "https://unbl0ck.github.io/games/papas/papasfreezeria/index.html",
+    "image": "https://unbl0ck.github.io/games/img/papasfreezeria.png"
+}, {
+    "name": "Papa's Pizzeria",
+    "url": "https://unbl0ck.github.io/games/papas/papaspizzeria/index.html",
+    "image": "https://unbl0ck.github.io/games/img/papaspizzeria.png"
+}, {
+    "name": "1v1.LOL",
+    "url": "https://unbl0ck.github.io/games/1v1lol/index.html",
+    "image": "https://unbl0ck.github.io/games/img/1v1lol.png"
+}, {
+    "name": "Bloons TD 5",
+    "url": "https://unbl0ck.github.io/games/bloonstd5/index.html",
+    "image": "https://unbl0ck.github.io/games/img/bloonstd5.png"
+}, {
+    "name": "Fruit Ninja",
+    "url": "https://unbl0ck.github.io/games/fruitninja/index.html",
+    "image": "https://unbl0ck.github.io/games/img/fruitninja.png"
+}, {
+    "name": "Happy Wheels",
+    "url": "https://unbl0ck.github.io/games/happywheels/index.html",
+    "image": "https://unbl0ck.github.io/games/img/happywheels.png"
+}, {
+    "name": "Subway Surfers",
+    "url": "https://unbl0ck.github.io/games/subwaysurfers/index.html",
+    "image": "https://unbl0ck.github.io/games/img/subwaysurfers.png"
+}, {
+    "name": "Paper.io 2",
+    "url": "https://unbl0ck.github.io/games/paperio2/index.html",
+    "image": "https://unbl0ck.github.io/games/img/paperio2.png"
+}, {
+    "name": "The Binding of Isaac",
+    "url": "https://unbl0ck.github.io/games/thebindingofissac/index.html",
+    "image": "https://unbl0ck.github.io/games/img/thebindingofissac.png"
+}, {
+    "name": "Portal: The Flash Version",
+    "url": "https://unbl0ck.github.io/games/portalflash/index.html",
+    "image": "https://unbl0ck.github.io/games/img/portalflash.png"
+}, {
+    "name": "Jetpack Joyride",
+    "url": "https://unbl0ck.github.io/games/jetpackjoyride/index.html",
+    "image": "https://unbl0ck.github.io/games/img/jetpackjoyride.png"
+}, {
+    "name": "BitLife",
+    "url": "https://unbl0ck.github.io/games/bitlife/index.html",
+    "image": "https://unbl0ck.github.io/games/img/bitlife.png"
+}];
